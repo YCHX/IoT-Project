c0af8372d118178d0d3c7bd5bc203872e8aaf4be
YCHX <ychx.alvin@gmail.com>
2021-06-16 00:11:10 +0900

    Link Sync with Notify

--- Notify.py
+++ Notify.py
@@ -7,8 +7,12 @@ def send_notify(notification_message):
     
     line_notify_token = 'ywdBpwRENoeGwzAAIEdToLGUXo2P2PlbtlKuWRmpJP0'
     line_notify_api = 'https://notify-api.line.me/api/notify'
-    headers = {'Authorization': f'Bearer {line_notify_token}'}
-    data = {'message': f'message: {notification_message}'}
+    #For python3.6 and after
+    #headers = {'Authorization': f'Bearer {line_notify_token}'}
+    #data = {'message': f'message: {notification_message}'}
+    #For others
+    headers = {'Authorization': 'Bearer '+ line_notify_token}
+    data = {'message': 'message: ' + notification_message}
     requests.post(line_notify_api, headers = headers, data = data)
 
 if __name__ == "__main__":
