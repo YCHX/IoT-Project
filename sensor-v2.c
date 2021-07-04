dca81e26a56657dca3fc5df52961326723c49261
YCHX <ychx.alvin@gmail.com>
2021-07-04 21:40:15 +0900

    v1.1 Don't need init any more

--- sensor-v2.c
+++ sensor-v2.c
@@ -5,10 +5,10 @@
 #include <errno.h>
 
 #define DATA_SIZE 65535
-#define TRIGGER_LEVEL 13
-#define ZERO_TRIGGER_LEVEL 6
+#define TRIGGER_LEVEL 20
+#define ZERO_TRIGGER_LEVEL 12
 #define BOOSTED_TRIGGER_LEVEL 20
-#define JUDGE_LEVEL 30
+#define JUDGE_LEVEL 40
 
 int _judge(float ap1, float ap2, float ap3){
     if ((ap1 < ap3)&&(ap3 < ap2)){
@@ -17,19 +17,7 @@ int _judge(float ap1, float ap2, float ap3){
     if ((ap2 < ap3)&&(ap3 < ap1)){
         return 2;
     }
-    // if ((ap3 >= ap1)&&(ap1 > ap2)){
-    //     return 2;
-    // }
-    // if ((ap3 >= ap2)&&(ap2 > ap1)){
-    //     return 1;
-    // }
-    // if ((ap3 <= ap1)&&(ap1 < ap2)){
-    //     return 1;
-    // }
-    // if ((ap3 <= ap2)&&(ap2 < ap1)){
-    //     return 2;
-    // }
-    return 0;
+    return 3;
 }
 
 void clear(int data[DATA_SIZE]){
@@ -38,17 +26,13 @@ void clear(int data[DATA_SIZE]){
     }
 }
 
-int judge(int data[DATA_SIZE], int count){
+int judge(int data[DATA_SIZE], int count, int dominate){
     float ap1 = 0, ap2 = 0, ap3 = 0;
-    int c1 = 0, c2 = 0, c3 = 0;
-    if (data[0] == 1){
-        return 1;
-    }
-    if (data[0] == 2){
-        return 2;
-    }
+    int c0 = 0, c1 = 0, c2 = 0, c3 = 0;
     for (int i = 0; i < count; i++){
         switch (data[i]){
+            case 0:
+                c0++;
             case 1:
                 c1++;
                 ap1 += i;
@@ -64,6 +48,15 @@ int judge(int data[DATA_SIZE], int count){
             default:
             break;
         }
+    }
+    if (c0 > JUDGE_LEVEL / 2) {
+        return 0;
+    }
+    if (dominate == 1){
+        return 1;
+    }
+    if (dominate == 2){
+        return 2;
     }
     ap1 /= (float)c1;
     ap2 /= (float)c2;
@@ -95,6 +88,7 @@ int main(void){
     printf("\n");
     printf("If there are objects, the sensor may give a wrong feedback\n");
     printf("\n");
+    before = 0;
     // while (1){
     //     printf(". . .");
     //     usleep(10000);
@@ -107,58 +101,60 @@ int main(void){
     printf("Initialization Complete!\n");
 
     while (1){
+    
     if (count >= DATA_SIZE){
         printf("data overflow\n");
         //Overflow dealing
+        return 0;
 
     } else if (count >= JUDGE_LEVEL){
-        dominate = judge(data, count);
+        dominate = judge(data, count, dominate);
         if (dominate == 1){
             printf("enter\n");
-            wait = 1;
-            boost = 0;
             clear(data);
             count = 0;
             interval = 0;
+            trigger = 0;
+            dominate = 0;
+            dominate_flag = 0;
         }else if (dominate == 2){
             printf("leave\n");
-            wait = 1;
-            boost = 0;
             clear(data);
             count = 0;
             interval = 0;
+            trigger = 0;
+            dominate = 0;
+            dominate_flag = 0;
+        }else if (dominate == 3){
+            //cannot judge, continue
         }else{
-            
+            clear(data);
+            count = 0;
+            trigger = 0;
+            dominate = 0;
+            dominate_flag = 0;
         }
         
     }
+
     //***SLEEP*** (Significant because if no sleep the system will hang up)
-    if (boost){
-        usleep(50000);
-    }else {
-        usleep(100000);
-    }
+    
+        usleep(20000);
+    
     if (read(fd, buf,4)<0) perror("read");
     printf("%s",buf);
 
     if (buf[0]!='e') {
-        interval = 0;
-        if (wait) {
-            data[count] = buf[0] + '0';
-            before = data[count];
-            if (buf[0] == '0'){
-                zero_trigger = 1;
-            }
-            count++;
-            continue;
-        }
         if (trigger){
-            boost = 1;
-            trigger = 0;
-            dominate_flag = 1;
+            if (interval > TRIGGER_LEVEL){
+                dominate_flag = 1;
+            }
             clear(data);
             count = 0;
         }
+
+        interval = 0;
+        
         
         
         
@@ -167,46 +163,40 @@ int main(void){
         {
         case '0':
             if (dominate_flag){
-                dominate = -1;
                 dominate_flag = 0;
             }
             data[count] = 0;
-            before = data[count];
+            before = 0;
             count++;
-            zero_trigger = 1;
+
             break;
         case '1':
             if (dominate_flag){
                 dominate = 1;
-                dominate_flag = 0;
             }
             data[count] = 1;
-            before = data[count];
+            before = 1;
             count++;
-            zero_trigger = 0;
+
             break;
         case '2':
             if (dominate_flag){
                 dominate = 2;
-                dominate_flag = 0;
             }
             data[count] = 2;
-            before = data[count];
+            before = 2;
             count++;
-            zero_trigger = 0;
 
             break;
         case '3':
             if (dominate_flag){
-                dominate = -1;
                 dominate_flag = 0;
             }
             data[count] = 3;
-            before = data[count];
+            before = 3;
             count++;
-            zero_trigger = 0;
+
             break;
-        
         default:
             break;
         }
@@ -214,39 +204,20 @@ int main(void){
         }else{
             interval++;
             printf("-%d-",interval);
-            if (zero_trigger){
+            if (trigger){
+                data[count] = before;
+                count++;
+            }
+            if (before == 0){
                 if (interval > ZERO_TRIGGER_LEVEL){
                     trigger = 1;
-                    wait = 0;
-                    zero_trigger = 0;
                 }
             }else{
                 if (interval > TRIGGER_LEVEL){
                     trigger = 1;
-                    wait = 0;
                 }
             }
-            if (boost){
-                if (interval > BOOSTED_TRIGGER_LEVEL){
-                    if (!wait){
-                        dominate = judge(data, count);
-                        if (dominate == 1){
-                            printf("enter\n");
-                        }else if (dominate == 2){
-                            printf("leave\n");
-                        }
-                        trigger = 1;
-                        boost = 0;
-                        clear(data);
-                        count = 0;
-                        interval = 0;
-                    }
-                }else{
-                    data[count] = before;
-                    count++;
-                }
-            }
-
+            
         }
         
 
